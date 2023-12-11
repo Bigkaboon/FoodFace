5d1f82b39ba66f047128fea6bac59970ff9f9c15
Bigkaboon <>
2023-12-11 11:08:12 +0000

    update

--- foodface/settings.py
+++ foodface/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-bigkaboon-foodface-dlpmczf5xz.us2.codeanyapp.com', 'foodface.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-bigkaboon-foodface-dlpmczf5xz.us2.codeanyapp.com', 'foodface-a39d2becae0b.herokuapp.com', 'localhost']
 
 
 
