cd31a08f214b3fa6c25279dc130744f85435ad6d
Dexter Törnqvist Saramies <juicebravo1@gmail.com>
2024-02-27 10:51:33 +0000

    Styled Edit/Delete btns on Posts - Styled PostForm

--- blog/forms.py
+++ blog/forms.py
@@ -11,6 +11,25 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ('title', 'content', 'excerpt', 'featured_image')
 
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'title': 'Title',
+            'content': 'Content - Write the content of your post here',
+            'excerpt': 'Excerpt'
+        }
+
+        for field in self.fields:
+            if field != 'featured_image':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'post-form-style'
+            self.fields[field].label = False
+
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
